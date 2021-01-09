ba469e802419561a798ca72ee1c8f51dd937f7b3
TechyGuy <teo.lund1523@gmail.com>
2021-01-09 16:14:17 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,27 @@
+var api = 'https://explorer.gamersnest.org/api';
+var donationAddress = "gncNiJKiLaQS5orggVHyqwdjzKAgPUpsJCPv4VFywN3d1G3FM6eBhLB3vbSUrmtjawBNwwvL1vvZ4AJfffVkwHYu6Y2Jub1GTn";
+var blockTargetInterval = 60;
+var coinUnits = 100000;
+var totalSupply =  1000000000;
+var symbol = 'GNC';
+var refreshDelay = 30000;
+
+
+const ExplorerConfig = {
+    apiBaseUrl: 'https://explorer.gamersnest.org/api'
+  }
+
+// pools stats
+var networkStat = {
+ "gnc": [
+    ["Gamersnest", "https://coins.gamersnest.org"],
+   
+ ]
+};
+
+
+var networkStat2 = {
+    "gnc": [
+
+    ]
+   };
